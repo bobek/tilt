fad8bd45b1619c04ccb43d79614534448d7a7c18
Antonin Kral <a.kral@bobek.cz>
2024-03-21 16:23:50 +0100

    Filter out some options

--- lib/tilt/pandoc.rb
+++ lib/tilt/pandoc.rb
@@ -2,17 +2,24 @@
 require_relative 'template'
 require 'pandoc-ruby'
 
+
+# some options are not recognized by pandoc
+OPTIONS_TO_DROP = [:outvar, :context, :fenced_code_blocks, :keep_separator]
+
 # Pandoc markdown implementation. See: http://pandoc.org/
 Tilt::PandocTemplate = Tilt::StaticTemplate.subclass do
   # turn options hash into an array
   # Map tilt options to pandoc options
   # Replace hash keys with value true with symbol for key
   # Remove hash keys with value false
+  # Remove prohibited keys
   # Leave other hash keys untouched
   pandoc_options = []
   from = "markdown"
   smart_extension = "-smart"
   @options.each do |k,v|
+    continue if OPTIONS_TO_DROP.include?(k)
+
     case k
     when :smartypants
       smart_extension = "+smart" if v
